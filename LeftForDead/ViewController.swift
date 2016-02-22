409db136c8225d34b00d54c0b872b100917bce4d
chtmorris <agoosester@gmail.com>
2016-02-22 11:28:14 +0800

    Initial Commit

---
+++ LeftForDead/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeftForDead
+//
+//  Created by Charlie Morris on 22/02/2016.
+//  Copyright © 2016 Mind Fund Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
